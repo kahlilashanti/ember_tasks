36d57e1131f000eb5efc2ef656bcd9a69ecda09f
Kahlil Ashanti <kahlilashanti2@Kahlils-MacBook-Air.local>
2017-04-30 17:31:17 -0700

    added a template and controllers for tasks

---
+++ app/controllers/tasks/new.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  //this is where we put the action function for the submit button
+  actions: {
+    addTask: function(){
+      //each of the fields in the form need to be stored in separate variables
+      //this is the variable for the title field
+      var title = this.get('title');
+      var description = this.get('description');
+      var date = this.get('date');
+      //we test this out by using alert (similar to console.log)
+      //alert(title);
+
+      //create new task
+      //var newTask = this.store.createRecord();
+    }
+  }
+
+});
